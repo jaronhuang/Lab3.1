887a1a0eab590c1eb92eebcbec96bf7b49c3883b
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-11-08 13:16:10 -0500

    11/8

---
+++ Lab3.1/src/ArrayMethods.java
@@ -0,0 +1,53 @@
+
+public class ArrayMethods 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public static int[ ] removeDuplicates(int[] list)
+	{
+		int[] cList;
+		int c = 0;
+		
+		for (int i = 0; i < list.length - 1; i++)
+		{
+			for (int j = 1; j <list.length; j++)
+			{
+				if (list[i] != list[j])
+				{
+					
+				}
+			}
+		}
+		
+		cList = new int[c];
+
+		for(int i = 0; i < list.length - 1; i++)
+		{
+			for(int j = 1; j <list.length; j++)
+			{
+				if (list[i] != list[j])
+				{
+					
+				}
+			}
+		}
+	}
+	
+	public static int[][] productArray(int[] arr1, int[] arr2)
+	{
+	
+	}
+	
+	public static int[ ][ ] pascalTriangle(int n)
+	{
+		
+	}
+	
+	public static void printPascalTriangle(int[ ] [ ] triangle)
+	{
+		
+	}
+}
